d9e61d865c21f675e4116d821bf7d21506af5af8
TommasoBernardo <tommasob1210@gmail.com>
2023-01-24 15:39:15 +0100

    riceved dates

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php echo 'ciao';
+<?php //; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <form action="./ricevi.php" method="POST">
+            <label for="">inserisci il tuo nome</label>
+            <input type="text" name="nome" placeholder="Example giggino">
+            <label for="">inserisci la tua email</label>
+            <input type="email" name="email" placeholder="ex gig23@mail.com">
+            <button type="submit">invia</button>
+        </form>
+    </main>
+</body>
+
+</html>
---
+++ ricevi.php
@@ -0,0 +1,11 @@
+<h2>
+    <?php
+    echo $_POST['nome'];
+    ?>
+</h2>
+
+<h5>
+    <?php
+    echo $_POST['email'];
+    ?>
+</h5>
